73a71e2dc93312e5138ca188fe208b4dfcfffea2
Natsu <phamdatytb2006@gmail.com>
2026-01-28 13:35:54 +0700

    Khởi động dự án

---
+++ src/main/java/com/fastfood/webff/WebffApplication.java
@@ -0,0 +1,13 @@
+package com.fastfood.webff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastfood/webff/WebffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfood.webff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
